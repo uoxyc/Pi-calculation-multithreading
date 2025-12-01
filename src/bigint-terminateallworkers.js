7abc7a6272d2b8876994bf9171845f4531210f2b
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-12-01 19:45:52 +0100

    Create bigint-terminateallworkers.js

---
+++ src/bigint-terminateallworkers.js
@@ -0,0 +1,9 @@
+import { myworker } from "./bigint-myworker";
+
+export function terminateallworkers() {
+    myworker.forEach(function (currentValue, index, arr) {
+        currentValue?.terminate();
+        // arr[index] = void 0;
+    });
+    myworker.length = 0;
+}
