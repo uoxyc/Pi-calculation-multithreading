0d8bde722a8e388d840c788689e123399e9449eb
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-12-01 20:27:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { bigintworkersetup } from "./bigintworkersetup.js"
 import { decimalworkersetup } from "./decimalworkersetup.js"
-import { terminateallworkers as terminateallworkers1 } from "./bigint-terminateallworkers"
-import { terminateallworkers as terminateallworkers2 } from "./decimal-terminateallworkers"
+import { terminateallworkers as terminateallworkers1 } from "./bigint-terminateallworkers.js"
+import { terminateallworkers as terminateallworkers2 } from "./decimal-terminateallworkers.js"
 export function bigintcleanup() {
   terminateallworkers1()
 }
@@ -9,7 +9,7 @@ export function bigintcleanup() {
 export function decimalcleanup() {
   terminateallworkers2()
 }
-export { decimalworkersetup }
-export { bigintworkersetup }
+export { decimalworkersetup } from "./decimalworkersetup.js"
+export { bigintworkersetup } from "./bigintworkersetup.js"
 export { bigintCalculatePi } from "./bigintCalculatePi.js"
 export { decimalCalculatePi } from "./decimalCalculatePi.js"
