3e92d8c11d7418cea07e7b5ace8b38b2c4c533ae
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-12-01 19:47:12 +0100

    Create bigintCalculatePi.js

---
+++ src/bigintCalculatePi.js
@@ -0,0 +1,54 @@
+import bigInt from "big-integer";
+import { myworker } from "./bigint-myworker";
+import { add } from "./bigintextend";
+
+export async function bigintCalculatePi(
+    createworker,
+    piwei,
+    threadgeshu
+) {
+    var p,
+        x = 0;
+
+    p = bigInt(0);
+
+    for (var i = 0, len = threadgeshu; i < len; i++) {
+        if (myworker.length >= threadgeshu) {
+            break;
+        } else {
+            myworker.push(createworker());
+        }
+    }
+    console.log(myworker);
+    await Promise.all(
+        myworker.map(function (currentValue, index, arr) {
+            return new Promise((res, rej) => {
+                currentValue.onmessage = function (event) {
+                    console.log(
+                        "主线程从副线程" + (index + 1) + "接收" + "event.data\n"
+                    );
+                    console.log(
+                        "第一个参数",
+                        event.data[0],
+                        "\n第二个参数",
+                        event.data[1]
+                    );
+
+                    var p1 = bigInt(event.data[0]);
+
+                    p = add(p, p1);
+                    x = Math.max(x, parseInt(event.data[1]));
+                    res();
+                };
+                currentValue.onerror = (e) => {
+                    console.error("Error:", e.error, e.message);
+                    currentValue.terminate();
+                    rej(new Error(e.error + e.message));
+                };
+                currentValue.postMessage([piwei, threadgeshu, index]);
+            });
+        })
+    );
+    let resultpi = p.toString()[0] + "." + p.toString().slice(1);
+    return [resultpi, x];
+}
