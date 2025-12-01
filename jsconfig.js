2e3871d02fb1df1fe3ff2279663ff2111728b5d3
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-12-01 20:14:46 +0100

    Create jsconfig.js

---
+++ jsconfig.js
@@ -0,0 +1,17 @@
+{
+    "include": ["src", "vite.config.js"],
+    "compilerOptions": {
+        "moduleResolution": "node",
+        "skipDefaultLibCheck": true,
+        "skipLibCheck": true,
+        "allowJs": false,
+        "checkJs": false,
+        "types": ["vite/client"],
+        "removeComments": true,
+        "strict": true,
+        "declaration": true,
+        "esModuleInterop": true,
+        "target": "esnext",
+        "module": "esnext"
+    }
+}
