8a893ff43bcbe353c5d1481a8947bd50a1cd804e
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-12-01 20:02:06 +0100

    Create decimalworkersetup.js

---
+++ src/decimalworkersetup.js
@@ -0,0 +1,19 @@
+import { decimalchildwork } from "./decimalchildwork";
+
+export function decimalworkersetup() {
+    (() => {
+        addEventListener("message", function (event) {
+            var piwei;
+            piwei = event.data[0];
+            var threadall = event.data[1];
+            var threadid = event.data[2];
+            console.log(
+                "副线程" + (threadid + 1) + "从主线程接收" + "event.data\n"
+            );
+            console.log(...event.data);
+
+            const [p, x] = decimalchildwork(piwei, threadall, threadid);
+            postMessage([p, x]);
+        });
+    })();
+}
